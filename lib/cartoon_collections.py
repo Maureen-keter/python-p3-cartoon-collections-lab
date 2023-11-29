275fa232fd875274f0ef99c523cc8db323c0354f
maureen <maureen.chelangat@student.moringaschool.com>
2023-11-29 06:47:55 +0300

    completed assignment

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,29 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(dwarfs):
+    index=0
+    for dwarf in dwarfs:
+        print(f"{index +1}. {dwarf}")
+        index+=1
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(planeteers):
+    capitalized_planeteers=[planet.capitalize() + "!" for planet in planeteers]
+    return capitalized_planeteers
 
-def long_planeteer_calls():
-    pass
 
-def find_the_cheese():
-    pass
+def long_planeteer_calls(calls):
+        for call in calls:
+             if len(call)>4:
+                  return True
+        else:
+             return False          
+                           
+def find_the_cheese(ingredients):
+    cheese_types = ["cheddar", "gouda", "camembert"]
+    for ingredient in ingredients:
+        if any(cheese in ingredient for cheese in cheese_types):
+            return ingredient
+    return None
+
+         
+
+         
+   
